30b197a91d2918dd88c5ddf15fd811302672eca6
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-07-28 00:35:45 +0200

    styles compositions

--- sanity-medium-production/components/Hero.tsx
+++ sanity-medium-production/components/Hero.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className="flex border-b-2 px-4 border-t-2  border-solid border-black lg:px-[8em] justify-between  bg-yellow-400 h-[60vh]">
+    <div className="flex border-b-2 sm:px-4 border-t-2 px-1 border-solid border-black lg:px-[8em] justify-between  bg-yellow-400 h-[60vh]">
         <div className="flex flex-col gap-6 justify-center">
-            <p className=" font-serif text-[4em] lg:text-[8em] text-black">Stay Curios.</p>
+            <p className=" font-serif text-[2em] sm:text-[4em] lg:text-[8em] text-black">Stay Curios.</p>
             <p className="font-semibold pr-12 font-sans">Discover stories, thinking, and expertise from writers on any topic. The Medium publication logo is the image which appears at the top of all your publication's stories.  The Medium publication logo is the image which appears at the top of all your publication's stories. </p>
             <button className="btn bg-black text-white p-6 w-52 rounded-full drop-shadow-lg ">Start Reading</button>
         </div>
