990c963a8f45e02cb740b4c505a37497af918fd7
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-08-02 13:46:05 +0200

    the post component optimization

--- sanity-medium-production/components/PostCard.tsx
+++ sanity-medium-production/components/PostCard.tsx
@@ -8,22 +8,21 @@ type Props = {
 const PostCard:React.FC=({posts} : Props) => {
   // console.log("Post Card Data",posts)
   return (
-    <div className='bg-white max-h-screen gap-4 max-w-screen lg:px-[8em] justify-between grid grid-cols-4'>
+    <div className='bg-white py-5 max-h-screen gap-4 max-w-screen lg:px-[8em] justify-between grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4'>
      {posts.map((post) => (
-        <Link key={post._id} href={`/post/${post.slug.
-        current}`} className='bg-red max-h-[30vh] max-w-[30vw] '>
+        <div key={post._id} className='bg-red-500 rounded-sm max-h-[30vh] max-w-[30vw] '>
         <div className='bg-green max-h-1/2 w-full'>
             <img src={urlFor(post.mainImage).url()!} className='w-full h-full object-cover' alt="" />
         </div>
-        <div className='bg-white p-5'>
+        <div className='bg-red-500 p-5'>
           <h1>{post.title}</h1>
           <p>{post.description}</p>
           <div className="flex gap-6 ">
-              <img src={urlFor(post.author.image).url()!}className='max-w-24 max-h-24 rounded-full' alt="" />
+              <img src={urlFor(post.author.image).url()!}className='max-w-12 max-h-10 rounded-full' alt="" />
             <span className='p-4'>{post.author.name}</span>
           </div>
         </div>
-    </Link>
+    </div>
      ))}
     </div>
   )
